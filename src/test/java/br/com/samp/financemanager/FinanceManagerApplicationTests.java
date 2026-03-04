608f59e793021721188cf9384174a0a31bd0ec29
Jonatas Sampaio <aurijona192@gmail.com>
2026-03-04 04:28:22 -0300

    Initial commit

---
+++ src/test/java/br/com/samp/financemanager/FinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.samp.financemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
