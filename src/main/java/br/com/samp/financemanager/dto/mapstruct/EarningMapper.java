764dcc7c22ed749b1e418505289a6da0a3c09023
Jonatas Sampaio <aurijona192@gmail.com>
2026-03-18 23:52:18 -0300

    add toEntity in EarningMapper

--- src/main/java/br/com/samp/financemanager/dto/mapstruct/EarningMapper.java
+++ src/main/java/br/com/samp/financemanager/dto/mapstruct/EarningMapper.java
@@ -1,11 +1,26 @@
 package br.com.samp.financemanager.dto.mapstruct;
 
+import br.com.samp.financemanager.dto.request.EarningRequest;
 import br.com.samp.financemanager.dto.response.EarningResponse;
 import br.com.samp.financemanager.model.Earning;
+import br.com.samp.financemanager.model.Expense;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface EarningMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "categories", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    Earning toEntity(EarningRequest request);
+
     EarningResponse toResponse(Earning earning);
+
+    List<EarningResponse> toResponseList(List<Earning> earnings);
+
+
 }
