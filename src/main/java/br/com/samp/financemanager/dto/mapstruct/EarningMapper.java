d744959f4c077b9f10003fefa3760242f4c78388
Jonatas Sampaio <aurijona192@gmail.com>
2026-03-24 23:45:49 -0300

    complete EarningMapper

--- src/main/java/br/com/samp/financemanager/dto/mapstruct/EarningMapper.java
+++ src/main/java/br/com/samp/financemanager/dto/mapstruct/EarningMapper.java
@@ -3,13 +3,18 @@ package br.com.samp.financemanager.dto.mapstruct;
 import br.com.samp.financemanager.dto.request.EarningRequest;
 import br.com.samp.financemanager.dto.response.EarningResponse;
 import br.com.samp.financemanager.model.Earning;
-import br.com.samp.financemanager.model.Expense;
+import br.com.samp.financemanager.model.enums.TransactionStatus;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+import java.time.LocalDate;
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+import static br.com.samp.financemanager.model.enums.TransactionStatus.PENDING_CONFIRMATION;
+import static br.com.samp.financemanager.model.enums.TransactionStatus.PLANNED;
+
+@Mapper(componentModel = "spring",uses = CategoryMapper.class)
 public interface EarningMapper {
 
     @Mapping(target = "id", ignore = true)
@@ -18,9 +23,25 @@ public interface EarningMapper {
     @Mapping(target = "status", ignore = true)
     Earning toEntity(EarningRequest request);
 
+    @Mapping(source = ".",target = "status",qualifiedByName = "calculateStatus")
     EarningResponse toResponse(Earning earning);
 
     List<EarningResponse> toResponseList(List<Earning> earnings);
 
+
+    @Named("calculateStatus")
+    default TransactionStatus calculateStatus(Earning earning) {
+        TransactionStatus status = earning.getStatus();
+
+        if (status!= PLANNED) {
+            return status;
+        }
+
+        if (earning.getDate().isBefore(LocalDate.now())){
+            return PENDING_CONFIRMATION;
+        }
+
+        return PLANNED;
+    }
 
 }
