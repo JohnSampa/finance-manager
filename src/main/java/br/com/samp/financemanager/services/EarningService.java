df00fd4b4a267e1053cb68a295d5c09eaed2e165
Jonatas Sampaio <aurijona192@gmail.com>
2026-03-24 23:57:03 -0300

    implement findById and findAll in EarningService

--- src/main/java/br/com/samp/financemanager/services/EarningService.java
+++ src/main/java/br/com/samp/financemanager/services/EarningService.java
@@ -1,12 +1,34 @@
 package br.com.samp.financemanager.services;
 
+import br.com.samp.financemanager.dto.mapstruct.EarningMapper;
+import br.com.samp.financemanager.dto.response.EarningResponse;
+import br.com.samp.financemanager.exceptions.ResourceNotFoundException;
+import br.com.samp.financemanager.model.Earning;
 import br.com.samp.financemanager.repository.EarningRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class EarningService {
 
     @Autowired
     private EarningRepository earningRepository;
 
+    @Autowired
+    private EarningMapper earningMapper;
 
+    public EarningResponse findById(Long id) {
+        Earning earning = earningRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Earning not found with id: " + id));
+
+        return earningMapper.toResponse(earning);
+    }
+
+    public List<EarningResponse> findAll() {
+        List<Earning> earnings = earningRepository.findAll();
+
+        return  earningMapper.toResponseList(earnings);
+    }
 }
