7e4d020d305f1c842e5ee3a0e50a95337f6f5b8f
Jonatas Sampaio <aurijona192@gmail.com>
2026-03-29 05:34:58 -0300

    Implementing a JWT token generation method in TokenService

--- src/main/java/br/com/samp/financemanager/infrastructure/security/jwt/TokenService.java
+++ src/main/java/br/com/samp/financemanager/infrastructure/security/jwt/TokenService.java
@@ -1,8 +1,34 @@
 package br.com.samp.financemanager.infrastructure.security.jwt;
 
+import br.com.samp.financemanager.exceptions.BusinessException;
+import br.com.samp.financemanager.model.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
 
 @Service
 public class TokenService {
 
+    @Value("S{application.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.create()
+                    .withSubject(user.getId().toString())
+                    .withIssuer("finance-manager")
+                    .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+                    .withClaim("role", user.getRole().toString())
+                    .sign(algorithm);
+        }catch (JWTCreationException e){
+            throw new BusinessException("Error generating token " + e.getMessage());
+        }
+    }
 }
