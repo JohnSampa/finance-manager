98fcefc9a13d22eec2c38c7e8e69075a576df0b7
Jonatas Sampaio <aurijona192@gmail.com>
2026-04-06 05:12:43 -0300

    Add login attempt counting and custom exception handling in AuthenticationService

--- src/main/java/br/com/samp/financemanager/auth/AuthenticationService.java
+++ src/main/java/br/com/samp/financemanager/auth/AuthenticationService.java
@@ -6,13 +6,26 @@ import br.com.samp.financemanager.auth.dto.AuthenticationDTO;
 import br.com.samp.financemanager.dto.mapstruct.UserMapper;
 import br.com.samp.financemanager.dto.request.UserRequest;
 import br.com.samp.financemanager.dto.response.UserResponse;
+import br.com.samp.financemanager.exceptions.AccountDisableException;
+import br.com.samp.financemanager.exceptions.AccountLockedException;
+import br.com.samp.financemanager.exceptions.InvalidCredentialsException;
 import br.com.samp.financemanager.infrastructure.security.jwt.TokenService;
 import br.com.samp.financemanager.model.User;
+import br.com.samp.financemanager.repository.UserRepository;
 import br.com.samp.financemanager.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalField;
 
 @Service
 public class AuthenticationService {
@@ -29,18 +42,53 @@ public class AuthenticationService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public AuthLoginResponse authenticate(AuthenticationDTO authDto) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(
                 authDto.email(),
                 authDto.password()
         );
 
-        var authentication = authenticationManager.authenticate(usernamePassword);
+        try {
+            var authentication = authenticationManager.authenticate(usernamePassword);
 
-        var token = tokenService.generateToken((User) authentication.getPrincipal());
+            User user = (User) authentication.getPrincipal();
 
-        return new AuthLoginResponse(token,"Bearer");
+            user.setFailedLoginAttempts(0);
 
+            user.setLockUntil(null);
+
+            user = userRepository.save(user);
+
+            var token = tokenService.generateToken(user);
+
+            return new AuthLoginResponse(token, "Bearer");
+        } catch (BadCredentialsException|UsernameNotFoundException exception) {
+
+            userRepository.findUserByEmail(authDto.email()).ifPresent(user -> {
+
+                user.registerFailedLoginAttempt();
+
+                userRepository.save(user);
+
+            });
+
+            throw new InvalidCredentialsException();
+        }catch (LockedException exception){
+            User user = userRepository.findUserByEmail(authDto.email())
+                    .orElse(null);
+
+            long seconds = 0;
+
+            if (user != null)
+                seconds = user.getRemainingLockSeconds();
+
+            throw new AccountLockedException(seconds);
+        }catch (DisabledException exception){
+            throw new AccountDisableException();
+        }
     }
 
     public AuthRegisterResponse register(UserRequest userRequest) {
@@ -49,8 +97,8 @@ public class AuthenticationService {
         var token = tokenService.generateToken(user);
 
         UserResponse userResponse = userMapper.toUserResponse(user);
-        AuthLoginResponse authLoginResponse = new AuthLoginResponse(token,"Bearer");
+        AuthLoginResponse authLoginResponse = new AuthLoginResponse(token, "Bearer");
 
-        return new AuthRegisterResponse(userResponse,authLoginResponse);
+        return new AuthRegisterResponse(userResponse, authLoginResponse);
     }
 }
--- src/main/java/br/com/samp/financemanager/repository/UserRepository.java
+++ src/main/java/br/com/samp/financemanager/repository/UserRepository.java
@@ -4,7 +4,11 @@ import br.com.samp.financemanager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     UserDetails findByEmail(String email);
+
+    Optional<User> findUserByEmail(String email);
 }
